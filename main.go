29d82f56e68c268a5493c3dc2347c82a1b60c385
ayman <aymanrafeeque419@gmail.com>
2025-10-29 10:01:10 +0530

    Implements code to parse the file

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"log/slog"
 	"logGen/parser"
 )
 
 func main() {
-	line := `2025-10-26 11:16:12.840 | DEBUG | cache | host=web01 | request_id=req-ymuon4-1921 | msg="Connection established to replica"`
+	// line := `2025-10-26 11:16:12.840 | DEBUG | cache | host=web01 | request_id=req-ymuon4-1921 | msg="Connection established to replica"`
 
-	entry, err := parser.ParseLogEntry(line)
+	// entry, err := parser.ParseLogEntry(line)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("struct: %#v", entry)
+
+	entries, err := parser.ParseLogFiles()
 	if err != nil {
-		log.Fatal(err)
+		slog.Error("Error here", "error", err)
 	}
-	fmt.Printf("struct: %#v", entry)
-
+	fmt.Println(entries)
 }
--- parser/logParse.go
+++ parser/logParse.go
@@ -1,8 +1,12 @@
 package parser
 
 import (
+	"bufio"
 	"fmt"
+	"log/slog"
 	"logGen/model"
+	"os"
+	"path/filepath"
 	"regexp"
 	"time"
 )
@@ -34,4 +38,40 @@ func ParseLogEntry(line string) (*model.LogEntry, error) {
 		Raw:       match[0],
 	}
 	return &entry, nil
+}
+
+func ParseLogFiles() ([]model.LogEntry, error) {
+	var allEntries []model.LogEntry
+
+	files, err := os.ReadDir("./logs")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read directory : %v", err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		path := filepath.Join("./logs/", file.Name())
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("Skipping file %s due to error: %v\n", path, err)
+			continue
+
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			entry, err := ParseLogEntry(line)
+			if err != nil {
+				slog.Error("Error while parsing : ", "error", err)
+				continue
+			}
+			allEntries = append(allEntries, *entry)
+		}
+
+	}
+	return allEntries, nil
 }
